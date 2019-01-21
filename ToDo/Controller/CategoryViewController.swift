3d96c87846d2fe7098ebdc9a3a66d66b59b06160
Brian De Maio <bdemaio1@gmail.com>
2019-01-21 11:34:34 -0500

    Categories linkes with Items, category can be selected and see associated added Items after segue, with proper search functionality in Item table view

--- ToDo/Controller/CategoryViewController.swift
+++ ToDo/Controller/CategoryViewController.swift
@@ -86,7 +86,14 @@ class CategoryViewController: UITableViewController {
     
     //Mark - TableView Category Selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        performSegue(withIdentifier: "goToItems", sender: self)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
+    }
 }
--- ToDo/Controller/TodoListViewController.swift
+++ ToDo/Controller/TodoListViewController.swift
@@ -13,12 +13,16 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
 
     var itemArray = [Item]()
     
+    var selectedCategory : CategoryItem? {
+        didSet{
+            loadItems()
+        }
+    }
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadItems()
     }
     
     //Mark - Create Table View
@@ -72,6 +76,7 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
             
             newItem.title = addItemTextField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
 
             self.itemArray.append(newItem)
             self.saveItems()
@@ -93,7 +98,17 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
     }
     
     //Mark - Load Items
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,additionalPredicate])
+        }
+        else{
+            request.predicate = categoryPredicate
+        }
+        
         do{
             itemArray = try context.fetch(request)
         }
@@ -106,9 +121,9 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
     //Mark - Search Bar
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
